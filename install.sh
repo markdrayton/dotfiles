8ecad4b1f154a498204c2e567918254308b7cfc3
Mark Drayton <mdrayton@gmail.com>
2019-12-23 19:00:35 +0100

    install: drop ssh-agent unit

--- install.sh
+++ install.sh
@@ -35,25 +35,3 @@ for file in "$target"/**/*; do
 done
 
 git ls-tree -r HEAD --name-only > "$target/.manifest"
-
-if [[ "$(ps -ocomm= 1)" == "systemd" ]]; then
-    echo "Adding ssh-agent systemd unit"
-    AGENT_UNIT=$HOME/.config/systemd/user/ssh-agent.service
-    if [[ ! -e "$AGENT_UNIT" ]]; then
-        mkdir -p "$(dirname "$AGENT_UNIT")"
-        cat << 'EOF' >> "$AGENT_UNIT"
-[Unit]
-Description=SSH key agent
-
-[Service]
-Type=forking
-Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
-ExecStart=/usr/bin/ssh-agent -a $SSH_AUTH_SOCK
-
-[Install]
-WantedBy=default.target
-EOF
-    fi
-    systemctl --user enable ssh-agent
-    systemctl --user start ssh-agent
-fi
