5b4a88a6e73cb5278fab8f3cd19b2cf2d42edecd
Mark Drayton <mdrayton@gmail.com>
2020-05-02 10:21:15 +0200

    zsh: copy ssh-agent config

--- .zprofile.d/010.ssh-agent-socket.sh
+++ .zprofile.d/010.ssh-agent-socket.sh
@@ -1,1 +1,4 @@
-/home/mark/.dotfiles/.bash_profile.d/010.ssh-agent-socket.sh
+# The systemd ssh-agent unit stashes the socket here
+if [ -e "$XDG_RUNTIME_DIR/ssh-agent.socket" ]; then
+    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+fi
