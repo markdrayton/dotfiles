e115f1543226b7f4c6fb2bb7d6402e9e06fbe0ce
Mark Drayton <mdrayton@gmail.com>
2020-07-27 22:24:50 +0200

    install: allow skipping files

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ shopt -s dotglob globstar nullglob
 
 [[ "$0" == /* ]] && target=$(dirname "$0") || target=$PWD
 
+touch dotfiles.skip
+
 if [[ -f "$target/.manifest" ]]; then
     comm -23 <(sort "$target/.manifest") \
             <(git ls-tree -r HEAD --name-only | sort) | while read -r del; do
@@ -20,8 +22,13 @@ fi
 for file in "$target"/**/*; do
     base="${file#$target/}"
     case "$base" in
-        .git|.gitignore|.git/*|.manifest|install.sh) continue ;;
+        .git|.gitignore|.git/*|.manifest|dotfiles.skip|install.sh) continue ;;
     esac
+
+    if [[ $(grep -sxF "$base" dotfiles.skip) ]]; then
+        echo "Skipping $base" >&2
+        continue
+    fi
 
     if [[ -d "$base" ]]; then
         mkdir -pv "$HOME/$base"
@@ -34,4 +41,4 @@ for file in "$target"/**/*; do
     fi
 done
 
-git ls-tree -r HEAD --name-only > "$target/.manifest"
+git ls-tree -r HEAD --name-only | grep -vsxFf dotfiles.skip > "$target/.manifest"
